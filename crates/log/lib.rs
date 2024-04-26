9ed8141f863ca2ac6eddacb5a9b816463192ac68
Anthony Weathersby <acweathersby.codes@gmail.com>
2024-04-26 12:25:44 -0600

    Add todo_note and dbg_prntln macros

--- crates/log/lib.rs
+++ crates/log/lib.rs
@@ -10,6 +10,26 @@ macro_rules! rum_log {
   }};
 }
 
+#[macro_export]
+macro_rules! todo_note {
+  ($($args: expr),*) => {
+    print!("⚠️  TODO: ");
+    print!($($args),*);
+    print!("\n  in file: {}:{}", file!(), line!());
+    println!(""); // to get a new line at the end
+  };
+}
+
+#[macro_export]
+macro_rules! dbg_println {
+  ($($args: expr),*) => {
+    {
+      #[cfg(debug_assertions)]
+      println!($($args),*);
+    }
+  };
+}
+
 /// Like `debug_assert!`, except completely compiled out in release
 /// builds
 #[macro_export]
